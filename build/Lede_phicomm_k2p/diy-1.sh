18d09a47993f76bc99786ee373627c0f1cbc1f68
8688Add <42204083+8688Add@users.noreply.github.com>
2020-11-16 23:40:53 +0800

    Update diy-1.sh

--- build/Lede_phicomm_k2p/diy-1.sh
+++ build/Lede_phicomm_k2p/diy-1.sh
@@ -25,3 +25,5 @@ rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/8688Add/luci-app-adbyby-plus-special.git package/luci-app-adbyby-plus-ram_edition-special
+rm -rf package/lean/luci-app-frpc
+git clone https://github.com/8688Add/luci-app-frpc-mod.git package/lean/luci-app-frpc
