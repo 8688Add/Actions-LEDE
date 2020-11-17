bc3e87a5d501682e388bb904fc02da16a38398c0
8688Add <42204083+8688Add@users.noreply.github.com>
2020-11-17 19:25:25 +0800

    Update diy-1.sh

--- build/Lede_phicomm_k2p/diy-1.sh
+++ build/Lede_phicomm_k2p/diy-1.sh
@@ -27,3 +27,4 @@ git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/8688Add/luci-app-adbyby-plus-special.git package/luci-app-adbyby-plus-ram_edition-special
 rm -rf package/lean/luci-app-frpc
 git clone https://github.com/8688Add/luci-app-frpc-mod.git package/lean/luci-app-frpc
+chmod 0755 package/lean/luci-app-frpc/root/etc/init.d/frp
