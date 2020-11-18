f777b2a71bff839fa5e797618175d9c1a08da539
8688Add <42204083+8688Add@users.noreply.github.com>
2020-11-18 20:44:45 +0800

    Update diy-1.sh

--- build/Lede_phicomm_k2p/diy-1.sh
+++ build/Lede_phicomm_k2p/diy-1.sh
@@ -26,5 +26,5 @@ git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/8688Add/adblock-plus.git package/luci-app-adblock-plus
 rm -rf package/lean/luci-app-frpc
-git clone https://github.com/8688Add/luci-app-frpc-mod.git package/luci-app-frpc
+git clone https://github.com/8688Add/luci-app-frpc-mod.git package/lean/luci-app-frpc
 chmod 0755 package/lean/luci-app-frpc/root/etc/init.d/frp
